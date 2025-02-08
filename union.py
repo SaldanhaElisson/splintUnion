d07a67260cfb9380221bb556a047ea5a511130ce
joselisboaa <joselisboaa05@gmail.com>
2025-02-08 14:40:24 -0300

    fix: exemplo banana maçã adaptado

--- union.py
+++ union.py
@@ -6,8 +6,6 @@ def split(string, delimiters):
     while i < len(string):
         match = False
         for delimiter in delimiters:
-            print(string[i:i + (len(delimiter))], delimiter)
-            print(string[i:i + len(delimiter)] == delimiter)
             if string[i:i + len(delimiter)] == delimiter:
                 result.append(current)
                 current = ""
@@ -22,19 +20,11 @@ def split(string, delimiters):
     return result
 
 
-def untion(stringValue):
-    caracter_especial = "|"
-    delimeters = []
-    pos_caracter_especial = 0
-    if stringValue:
-      for i in range(0, len(stringValue)):
-        if stringValue[i] == caracter_especial:
-            delimeters.append(stringValue[pos_caracter_especial:i])
-            pos_caracter_especial = i+1
-    return delimeters
+def union(stringValue):
+    return [delimiter.strip() for delimiter in stringValue.split("|") if delimiter.strip()]
 
 
-strinbSomeValue = "The quick brown fox jumps over the lazy dog. However, the fox was surprised to find a umdoistrêsquatrohidden treasure in the middle of the forest. The treasure was guarded by a fierce dragon, but the fox was clever and managed to outsmart the dragon. In the end, the fox shared the treasure with its friends, and they all lived happily ever after."
-
-delimeters = untion("um|dois|três|quatro|")
-print(split(strinbSomeValue, delimeters))
+string = "banana maçã"
+delimiters = union("na | ma")
+print(delimiters)
+print(split(string, delimiters))
